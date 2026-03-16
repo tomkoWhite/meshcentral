8576723d9de3dd723d8d44327c902f72452d5081
tomkoWhite <111019153+tomkoWhite@users.noreply.github.com>
2026-03-16 22:41:21 +0100

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 
 const path = require('path');
 const fs = require('fs');
-const Database = require('better-sqlite3');
+const Database = require('/usr/local/lib/node_modules/meshcentral/node_modules/better-sqlite3');
 
 const dataDir = path.join(__dirname, 'data');
 if (!fs.existsSync(dataDir)) {
