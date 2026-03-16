9cc5f0a2729dbd3992f062efec13bf357a4ade07
tomkoWhite <111019153+tomkoWhite@users.noreply.github.com>
2026-03-16 19:28:19 +0100

    Update botchat.js

--- botchat.js
+++ botchat.js
@@ -1,33 +1,20 @@
 "use strict";
 
+const path = require('path');
+
 module.exports.botchat = function (parent) {
     var obj = {};
     obj.parent = parent;
     obj.meshServer = parent.parent;
     obj.exports = [];
-    obj.VIEWS = __dirname + '/views/';
+    obj.VIEWS = path.join(__dirname, 'views');
 
     obj.server_startup = function () {
         console.log('=== BOTCHAT server_startup ===');
     };
 
     obj.handleAdminReq = function (req, res, user) {
-        res.setHeader('Content-Type', 'text/html; charset=utf-8');
-        res.end(`
-<!DOCTYPE html>
-<html lang="cs">
-<head>
-    <meta charset="utf-8">
-    <title>BotChat</title>
-    <style>
-        body { font-family: Arial, sans-serif; margin: 30px; }
-    </style>
-</head>
-<body>
-    <h1>BotChat</h1>
-    <p>Tady budou notifikace.</p>
-</body>
-</html>`);
+        res.sendFile(path.join(obj.VIEWS, 'botchat.html'));
     };
 
     return obj;
