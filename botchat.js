07ffe8b9c23a76f1a74a0dfe32a50da2209e7fbb
tomkoWhite <111019153+tomkoWhite@users.noreply.github.com>
2026-03-16 20:15:37 +0100

    Update botchat.js

--- botchat.js
+++ botchat.js
@@ -6,15 +6,35 @@ module.exports.botchat = function (parent) {
     var obj = {};
     obj.parent = parent;
     obj.meshServer = parent.parent;
-    obj.exports = [];
+    obj.exports = ['onWebUIStartupEnd', 'goPageEnd'];
     obj.VIEWS = path.join(__dirname, 'views');
 
     obj.server_startup = function () {
         console.log('=== BOTCHAT server_startup ===');
     };
 
+    // Přímá stránka BotChat
+    obj.hook_setupHttpHandlers = function () {
+        const app = obj.parent.webserver.app;
+
+        app.get('/plugins/botchat', function (req, res) {
+            res.sendFile(path.join(obj.VIEWS, 'botchat.html'));
+        });
+    };
+
+    // Stávající plugin admin page
     obj.handleAdminReq = function (req, res, user) {
         res.sendFile(path.join(obj.VIEWS, 'botchat.html'));
+    };
+
+    // Web UI hook po prvním načtení
+    obj.onWebUIStartupEnd = function () {
+        try { window.botchatAddSidebarButton(); } catch (e) { console.error(e); }
+    };
+
+    // Web UI hook po změně stránky
+    obj.goPageEnd = function () {
+        try { window.botchatAddSidebarButton(); } catch (e) { console.error(e); }
     };
 
     return obj;
